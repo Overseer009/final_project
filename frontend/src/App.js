a720f86864ebd767988e54c84a59dfe2028d70a3
markjwhite <markwhite259@gmail.com>
2021-07-04 12:39:17 -0400

    Add react component Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,48 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Form from './components/Form'
-
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Form from "./components/Form";
+import Login from "./components/logister/Login";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  
   const [stuff, setStuff] = useState();
 
   useEffect(() => {
     Promise.all([
-      axios.get('/api/users'),
-      axios.get('/api/instances'),
-      axios.get('/api/instance_colours'),
-      axios.get('/api/timelines')
-    ]).then(all => {
+      axios.get("/api/users"),
+      axios.get("/api/instances"),
+      axios.get("/api/instance_colours"),
+      axios.get("/api/timelines"),
+    ]).then((all) => {
       const [users, instances, instance_colours, timelines] = all;
-      console.log("line 21 ------ :", users.data)
+      console.log("line 21 ------ :", users.data);
       setStuff({
         users: users.data,
         instances: instances.data,
         instance_colours: instance_colours.data,
-        timelines: timelines.data
-    })
-    })
-  }, [])
+        timelines: timelines.data,
+      });
+    });
+  }, []);
 
-  const registerUser = async(name, email, password) => {
-    console.log("in reggy!")
+  const registerUser = async (name, email, password) => {
+    console.log("in reggy!");
     const userInfo = {
       name,
       email,
-      password
-    }
-    
-    axios.post(`/api/users`, userInfo)
-      .then(res => {
-        console.log("inside regeisterUser", res)
-      })
-  }
+      password,
+    };
 
-  
+    axios.post(`/api/users`, userInfo).then((res) => {
+      console.log("inside regeisterUser", res);
+    });
+  };
+
   return (
     <main className="App">
-      <header className="App-header">
-      {stuff ? <h1>we are in {stuff.users.name}</h1> : <h1>"hello"</h1>}
-      </header>
-      <div>
-        <Form
-          registerUser={registerUser}
-        />
+      <div className="logger">
+        <Login />
       </div>
     </main>
   );
