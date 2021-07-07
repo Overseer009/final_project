dc901c3077eae8e0d18c4e1d1225fbe7aeecfe9f
Overseer009 <sacha.sunnasy@gmail.com>
2021-07-07 13:34:14 -0400

    add function to be able to map the timeline

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -4,8 +4,8 @@ import TimelineItem from "./TimelineItem.jsx";
 
 const Timeline = function (props) {
   console.log(props.currentTimeline);
+
   const timelineBuilder = (start, end) => {
-    // console.log(timelineData);
     const months = {
       1: "January",
       2: "February",
@@ -20,16 +20,36 @@ const Timeline = function (props) {
       11: "November",
       12: "December",
     };
-
-    // const start = months{timelineData.start_month}
+  
+    let newTimeline = [];
+  
+    if (start > end) {
+      for (let i = start; i <= 12; i++) {
+        newTimeline.push(months[i])
+      };
+      for (let i = 1; i <= end; i++) {
+        newTimeline.push(months[i])
+      };
+      return newTimeline
+    } else {
+      for (let i = start; i < end + 1; i++) {
+        newTimeline.push(months[i]);
+      };
+      return newTimeline;
+    };
   };
 
-  //   timelineBuilder(props.currentTimeline)
+  const newTimeline = timelineBuilder(props.currentTimeline.start_month, props.currentTimeline.end_month)
+  console.log(newTimeline);
 
   return (
     <div className="timeline-container">
-      <span>something here</span>
-      {/* mapping data  */}
+      <span>{props.currentTimeline.name}</span>
+      {newTimeline.map((months) => {
+        console.log(months);
+        return <TimelineItem />
+      })}
+      {/* <TimelineItem />
       <TimelineItem />
       <TimelineItem />
       <TimelineItem />
@@ -37,9 +57,7 @@ const Timeline = function (props) {
       <TimelineItem />
       <TimelineItem />
       <TimelineItem />
-      <TimelineItem />
-      <TimelineItem />
-      <TimelineItem />
+      <TimelineItem /> */}
     </div>
   );
 };
